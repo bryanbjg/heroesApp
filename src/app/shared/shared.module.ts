4d461aab37d129fbf79b288b09ebe3b89522ba3a
Bryan Jimenez <bryanbg.jg@gmail.com>
2023-05-24 23:09:59 -0400

    add: new pages and modules

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { ErrorPageComponent } from './pages/error-page/error-page.component';
+
+
+
+@NgModule({
+  declarations: [
+    ErrorPageComponent
+  ],
+ 
+  exports:[
+    ErrorPageComponent
+  ]
+})
+export class SharedModule { }
