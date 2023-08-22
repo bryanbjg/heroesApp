0320b89f33b22e2543c6d15b2ba54068703896fb
Bryan Jimenez <bryanbg.jg@gmail.com>
2023-08-22 17:03:32 -0400

    add new service and new desing page

--- src/app/heroes/pages/heroes-page/heroes-page.component.ts
+++ src/app/heroes/pages/heroes-page/heroes-page.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HeroesService } from '../../services/heroes.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { switchMap } from 'rxjs';
+import { Hero } from '../../interfaces/hero.interface';
 
 @Component({
   selector: 'app-heroes-page',
   templateUrl: './heroes-page.component.html',
   styleUrls: ['./heroes-page.component.scss']
 })
-export class HeroesPageComponent {
+export class HeroesPageComponent implements OnInit {
+  public hero?: Hero
+  constructor(private heroesService: HeroesService, private activatedRoute: ActivatedRoute, private router: Router) { }
 
+  ngOnInit() {
+    this.activatedRoute.params
+      .pipe(
+        switchMap(({ id }) => this.heroesService.getHeroeById((id)))
+      ).subscribe(hero => {
+        if (!hero) return this.router.navigate(['/heroes/list']);
+        this.hero = hero;
+        return;
+      })
+  }
+
+  goBack(): void{
+    this.router.navigateByUrl('heroes/list')
+  }
 }
