d5ead6338e9325fe35d483ee15b992095822b7dc
Bryan Jiménez Guerrero <96020118+bryanbjg@users.noreply.github.com>
2024-02-03 20:08:33 -0400

    add: changes for create hero

--- src/app/heroes/pages/new-page/new-page.component.ts
+++ src/app/heroes/pages/new-page/new-page.component.ts
@@ -1,13 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
-import { Publisher } from '../../interfaces/hero.interface';
+import { Hero, Publisher } from '../../interfaces/hero.interface';
+import { HeroesService } from '../../services/heroes.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { switchMap } from 'rxjs';
 
 @Component({
   selector: 'app-new-page',
   templateUrl: './new-page.component.html',
   styleUrls: ['./new-page.component.scss']
 })
-export class NewPageComponent {
+export class NewPageComponent implements OnInit{
+
+  constructor(private heroService: HeroesService, private ActivatedRoute: ActivatedRoute, private router: Router){ }
+
+  get currentHero(): Hero{
+    const hero = this.heroForm.value as Hero;
+    return hero;
+  }
+
+  ngOnInit(): void {
+      if(!this.router.url.includes('edit')) return;
+
+      this.ActivatedRoute.params.pipe(
+        switchMap(({id}) => this.heroService.getHeroeById(id)),
+      ).subscribe(hero =>{
+        if(!hero) return this.router.navigateByUrl('/')
+        this.heroForm.reset()
+      return;
+      })
+  }
 
   public heroForm = new FormGroup({
     id: new FormControl<string>(''),
@@ -22,4 +44,18 @@ export class NewPageComponent {
     { id: 'DC Comics', value: 'DC - Comics' },
     { id: 'Marvel Comics', value: 'Marvel - Comics' }
   ];
+
+  onSubmit(): void{
+   if(this.heroForm.invalid) return
+    if(this.currentHero.id){
+      this,this.heroService.updateHero(this.currentHero).subscribe(hero =>{
+
+      });
+      return;
+    }
+
+    this.heroService.addHero(this.currentHero).subscribe(hero =>{
+
+    });
+  }
 }
