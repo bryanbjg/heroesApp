a0fe63ecddaff3df6849e97ca6d5b420c68a53c7
Bryan Jiménez Guerrero <96020118+bryanbjg@users.noreply.github.com>
2024-02-04 13:08:49 -0400

    feat: add authenticate and guards

--- src/app/auth/pages/login-page/login-page.component.ts
+++ src/app/auth/pages/login-page/login-page.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { AuthService } from '../../services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login-page',
@@ -6,5 +8,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./login-page.component.scss']
 })
 export class LoginPageComponent {
+constructor(private AuthService: AuthService, private router: Router){}
 
+  onLogin() : void{
+    this.AuthService.login('bryantrey@techsync.com', '123456').subscribe(user =>{
+      this.router.navigate(['/']);
+    })
+  }
 }
